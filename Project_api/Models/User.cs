139794e4c914c586570eb213a662bbbc5d2b69cc
20David2 <99254001+20David2@users.noreply.github.com>
2022-04-29 15:38:18 +0200

    db_created

---
+++ Project_api/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_api.Models
+{
+    public class User
+    {
+        public string userId { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+    }
+}
